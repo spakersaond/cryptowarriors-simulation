ab605b4c658e67126bcdbbe027e29c1438cf6a79
reacharddeter <reacharddeter@gmail.com>
2025-05-19 18:37:33 +0800

    Connect button set to overflow hidden

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -98,7 +98,7 @@ const Navbar = () => {
               </div>
             </div>
           </div>
-          <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+          <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0 overflow-hidden">
             <ConnectButton
               client={client}
               theme={lightTheme()}
